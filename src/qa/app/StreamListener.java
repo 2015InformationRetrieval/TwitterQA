11500ac5fc54a06e8d63294833567c86c15eb661
fujunswufe <fujunswufe@gmail.com>
2015-04-04 11:59:45 -0400

    test
    answer, user helper, stream listener

--- src/qa/app/StreamListener.java
+++ src/qa/app/StreamListener.java
@@ -1,5 +1,9 @@
 package qa.app;
 
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+import qa.app.Answer;
 import qa.connection.Parameter;
 import qa.service.UserService;
 import twitter4j.StallWarning;
@@ -10,7 +14,9 @@ import twitter4j.StatusUpdate;
 
 public class StreamListener implements StatusListener{
 	
+	String Neo4j_Path="/ir";
 	private UserService userService = new UserService();
+	GraphDatabaseService graphDataService=new GraphDatabaseFactory().newEmbeddedDatabase(Neo4j_Path);
 	@Override
 	public void onException(Exception arg0) {
 		// TODO Auto-generated method stub
@@ -43,7 +49,7 @@ public class StreamListener implements StatusListener{
 		
 		if(status.getUser().getId() != Long.parseLong(Parameter.USER_ID )){
 			if(userService.isExist(status.getUser())){
-				Answer.reply(status);
+				Answer.reply(status,graphDataService);
 			}else{
 				userService.createIndex(status.getUser());
 			}
