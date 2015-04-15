2cc661a48167fac792c9364d78f88ad7b7c83a49
chanlen <buptca@gmail.com>
2015-04-15 17:09:09 -0400

    update texttokenizer

--- src/qa/analysis/TextTokenizer.java
+++ src/qa/analysis/TextTokenizer.java
@@ -22,17 +22,21 @@ public class TextTokenizer {
 	String words = null;	
 	
 	// punctuation is the token delimiter
-    String punctuation = "*{}=[]()':_-?!/<>;., \\@";
-	
+   
 	// YOU MUST IMPLEMENT THIS METHOD
 	public TextTokenizer( String s ) {
 	// this constructor will tokenize the input texts (usually it is a char array for a whole document)
 		char[] texts = s.toLowerCase().toCharArray();
+		for(int i = 0;i < texts.length;i++){
+			if(texts[i] < 'a' || texts[i] > 'z'){
+				texts[i] = ' ';
+			}
+		}
 		words = new String(texts);
 		
 		// StringTokenizer is a class in util package, it can tokenize a large String by into several small Strings
 		// we can set up the token sign by ourselves;
-		StringTokenizer st = new StringTokenizer(words,punctuation);
+		StringTokenizer st = new StringTokenizer(words);
 		
 		String token = null;
 		
@@ -69,7 +73,7 @@ public class TextTokenizer {
 		return null;
 	}
 	public static void main(String args[]){
-		TextTokenizer t = new TextTokenizer("  What's pitts chis, sio?");
+		TextTokenizer t = new TextTokenizer("  ~~What's pitts chis, sio?");
 		System.out.println(t);
 		String a;
 		while((a = t.nextWord()) != null){
